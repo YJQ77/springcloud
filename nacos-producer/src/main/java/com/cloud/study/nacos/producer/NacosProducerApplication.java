4e1565ef6df7003e9b29d38daeeec9eeaa19dc0c
yinjiaqi <yinjiaqi17@163.com>
2021-04-07 16:04:53 +0800

    add gitignore

--- nacos-producer/src/main/java/com/cloud/study/nacos/producer/NacosProducerApplication.java
+++ nacos-producer/src/main/java/com/cloud/study/nacos/producer/NacosProducerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosProducerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NacosProducerApplication.class, args);
     }
